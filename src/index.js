6cb70a89e68c8aad0ebcb3fad341f6c3f67f6e78
Navruzbek <navruzbek.noraliev@gmail.com>
2020-03-11 17:24:45 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    //Replace all correct brackets and if true s is empty else it has not correct sequence
+    let t
+    if (parseFloat(str)) { // If numeric str
+        console.log('before',str)
+        do {
+            t = str
+            str = str.replace(/12|34|56|77|88/g, "");
+        } while (t.length != str.length);
+        console.log('after',str)
+        return str.length === 0
+    }
+    else {
+        do {
+            t = str
+            str = str.replace(/ *(\| *\||\{ *\}|\( *\)|\[ *\]) */g, "");
+        } while (t.length != str.length);
+        return str.length === 0;
+    }
 }
